8639e4b3861af118a8cec713658551774676d7dc
am2308 <b110020510@gmail.com>
2025-06-17 10:10:37 +0100

    Create index.ts

---
+++ frontend/src/components/Avatar3D/index.ts
@@ -0,0 +1,4 @@
+export { default as RealisticAvatar } from './RealisticAvatar';
+export { default as BabylonAvatar } from './BabylonAvatar';
+export { default as AvatarControls } from './AvatarControls';
+export { default as OutfitDetails } from './OutfitDetails';
